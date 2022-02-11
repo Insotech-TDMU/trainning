b35374a9044b055e519fad22b7ac91379625b56c
tam003 <nguyenbaotam003@gmail.com>
2022-02-11 09:17:04 +0700

    Update home_page.dart

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,89 +1,133 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-    return Container(
-      width: width,
-      height: height,
-      decoration: BoxDecoration(
-        color: Colors.white.withOpacity(0.8),
-        image: DecorationImage(
-          image: const AssetImage('./assets/images/home_background.jpg'),
-          colorFilter: ColorFilter.mode(Colors.white.withOpacity(0.65), BlendMode.dstATop),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
         body: Container(
-          width: double.infinity,
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                height: 80,
-                width: 80,
-                color: Colors.amberAccent,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://i.pinimg.com/736x/f1/33/f0/f133f0b924bbea125d045c4aab9b874f.jpg'),
+                fit: BoxFit.cover,
               ),
-
-              Row(
-                children: [
-                  InkWell(
-                    child: Column(
-                      children: [
-                        Container(
-                          width: 80,
-                          height: 80,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage('./assets/icons/chamsoc_icon.png'),
-                              fit: BoxFit.contain,
-                            )
-                          ),
+            ),
+            child: Column(children: [
+              Expanded(
+                flex: 2,
+                child: Container(
+                    padding: const EdgeInsets.only(top: 80),
+                    child: const Text('Osin',
+                        textAlign: TextAlign.end,
+                        style: TextStyle(fontSize: 55, color: Colors.red))),
+              ),
+              Expanded(
+                  flex: 5,
+                  child: Container(
+                      child: Column(children: [
+                    Container(
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                          InkWell(
+                              onTap: () {},
+                              child: Column(children: const [
+                                Icon(
+                                  FontAwesomeIcons.twitter,
+                                  size: 80,
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(top: 8.0),
+                                  child: Text('Chăm sóc'),
+                                )
+                              ])),
+                          InkWell(
+                              onTap: () {},
+                              child: Column(children: const [
+                                Icon(
+                                  FontAwesomeIcons.thumbsUp,
+                                  size: 80,
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(top: 8.0),
+                                  child: Text('Giúp việc'),
+                                )
+                              ])),
+                        ])),
+                    Container(
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                          InkWell(
+                              onTap: () {},
+                              child: Column(children: const [
+                                Icon(
+                                  FontAwesomeIcons.twitter,
+                                  size: 80,
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(top: 8.0),
+                                  child: Text('Nấu ăn'),
+                                )
+                              ])),
+                          InkWell(
+                              onTap: () {},
+                              child: Column(children: const [
+                                Icon(
+                                  FontAwesomeIcons.thumbsUp,
+                                  size: 80,
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(top: 8.0),
+                                  child: Text('Gia dụng
+                                  '),
+                                )
+                              ])),
+                        ])),
+                  ]))),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          color: Colors.white,
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Container(child: Text('>> Đăng ký:')),
+                                Container(child: Text('Khách hàng')),
+                                Container(child: Text('Cộng tác viên')),
+                              ]),
                         ),
-                        Text('Cham soc')
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              const Spacer(),
-              Container(
-                height: 60,
-                color: const Color(0xff7c0a02),
-                width: double.infinity,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: const [
-                    Text(
-                      'Bản quyền (C) 2016 thuộc INSOTECH',
-                      style: TextStyle(
-                        color: Colors.white,
                       ),
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Text(
-                      'Hỗ trợ Hotline: 0901.81.81.21',
-                      style: TextStyle(
-                        color: Colors.white,
+                      Expanded(
+                        child: Container(
+                            width: double.infinity,
+                            alignment: Alignment.center,
+                            color: Colors.red[900],
+                            child: const Text(
+                              'Bản quyền (C) 2016 thuộc về INSOTECH',
+                              textAlign: TextAlign.center,
+                            )),
                       ),
-                    )
-                  ],
+                      Container(
+                          width: MediaQuery.of(context).size.width,
+                          color: Colors.red[900],
+                          alignment: Alignment.center,
+                          child: const Text(
+                            'Hỗ trợ Hotline: 0901.81.81.21',
+                            textAlign: TextAlign.center,
+                          )),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+              )
+            ])));
   }
 }
